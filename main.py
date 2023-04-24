f9e56e3bc273ba604f76aaa6469cfb085cd1e852
unknown <doug.rokakis@gmail.com>
2023-04-24 14:49:45 -0400

    add csv to pdf converter app

---
+++ main.py
@@ -0,0 +1,35 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 20, 200, 20)
+    for i in range(20, 300, 10):
+        pdf.line(10, i, 200, i)  
+    
+    # Set the footer
+    pdf.ln(266)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+        for i in range(20, 300, 10):
+            pdf.line(10, i, 200, i)
+        # Set the footer
+        pdf.ln(278)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+
+pdf.output("output.pdf")
